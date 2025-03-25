68ae8f75284c2f2a64169b54923fedfb65ba95b9
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2025-03-25 10:44:15 +0100

    First model metric

--- a4s_eval/models/metrics.py
+++ a4s_eval/models/metrics.py
@@ -1,15 +1,82 @@
+import numpy as np
+import pandas as pd
+from pandas import Timestamp
 from sklearn.metrics import (
     accuracy_score,
+    f1_score,
+    matthews_corrcoef,
     precision_score,
     recall_score,
-    f1_score,
     roc_auc_score,
 )
 
-binary_metrics = {
-    "accuracy": accuracy_score,
-    "precision": precision_score,
-    "recall": recall_score,
-    "f1": f1_score,
-    "roc_auc": roc_auc_score,
+from a4s_eval.data_model.dataset import Feature
+from a4s_eval.data_model.metric import Metric
+from a4s_eval.data_model.project import Project
+from a4s_eval.utils.dates import DateIterator
+
+
+def robust_roc_auc_score(y_true: np.ndarray, y_pred_proba: np.ndarray) -> np.ndarray:
+    if y_pred_proba.shape[1] == 2:
+        y_pred_proba = y_pred_proba[:, 1]
+    return roc_auc_score(y_true, y_pred_proba)
+
+
+pred_classification_metric = {
+    "F1": f1_score,
+    "MCC": matthews_corrcoef,
+    "Accuracy": accuracy_score,
+    "Precision": precision_score,
+    "Recall": recall_score,
 }
+str2proba_classification_metric = {"ROCAUC": robust_roc_auc_score}
+
+
+def prediction_metric_test(
+    y_true: np.ndarray, y_pred_proba: np.ndarray, current_time: Timestamp
+) -> list[Metric]:
+    out = []
+    y_pred = np.argmax(y_pred_proba, axis=1)
+    for name, score_f in pred_classification_metric.items():
+        out.append(
+            Metric(
+                name=name,
+                score=score_f(y_true, y_pred),
+                time=current_time,
+            )
+        )
+
+    for name, score_f in str2proba_classification_metric.items():
+        out.append(
+            Metric(
+                name=name,
+                score=score_f(y_true, y_pred_proba),
+                time=current_time,
+            )
+        )
+
+    return out
+
+
+def prediction_test(
+    project: Project,
+    x_new: pd.DataFrame,
+    date_feature: Feature,
+    target_feature: Feature,
+    y_new_pred_proba: np.ndarray,
+) -> list[Metric]:
+    metrics = []
+    for end_date, x_curr in DateIterator(
+        date_round="1 D",
+        window=project.window_size,
+        freq=project.frequency,
+        df=x_new,
+        date_feature=date_feature.name,
+    ):
+        y_curr = x_curr[target_feature.name].to_numpy()
+        y_curr_pred_proba = y_new_pred_proba[x_curr.index]
+
+        metrics_curr = prediction_metric_test(y_curr, y_curr_pred_proba, end_date)
+        metrics.extend(metrics_curr)
+
+    return metrics
--- a4s_eval/tasks/evaluate_model.py
+++ a4s_eval/tasks/evaluate_model.py
@@ -1,11 +1,15 @@
+
+import numpy as np
+import onnxruntime as rt
 import pandas as pd
 import requests
 
 from a4s_eval.data_model.metric import Metric
 from a4s_eval.data_model.project import Project
-from a4s_eval.datasets.drift import data_drift_test
+from a4s_eval.models.metrics import prediction_test
 from a4s_eval.utils.env import API_URL
 from a4s_eval.utils.files import auto_get_read_dataset_file
+from a4s_eval.utils.logging import get_logger
 
 
 def post_metrics(project_id: int, metrics: list[Metric]) -> None:
@@ -14,13 +18,14 @@ def post_metrics(project_id: int, metrics: list[Metric]) -> None:
         json=[m.model_dump(mode="json") for m in metrics],
     )
     if response.status_code == 201:
-        print("Metrics sent successfully")
-        print(response.json())
+        get_logger().info("Metrics sent successfully")
     else:
-        print(f"Failed to send metrics {response.status_code}")
+        get_logger().error(f"Failed to send metrics {response.status_code}")
 
 
 def evaluate_dataset(project_id: int) -> None:
+    get_logger().info(f"Starting evaluation for project {project_id}")
+
     # Retrieve the data
 
     # Run the evaluations
@@ -39,21 +44,34 @@ def evaluate_dataset(project_id: int) -> None:
             x_new[project.dataset.date_feature.name]
         )
 
-        metrics = data_drift_test(
-            project,
-            x_ref,
-            x_new,
-            project.dataset.features,
-            project.dataset.date_feature,
+        x_test = x_new[[f.name for f in project.dataset.features]].to_numpy()
+
+        sess = rt.InferenceSession(
+            "random_forest.onnx", providers=["CPUExecutionProvider"]
         )
 
-        print(metrics)
+        input_name = sess.get_inputs()[0].name
+        label_name = sess.get_outputs()[1].name
+        pred_onx = sess.run([label_name], {input_name: x_test})[0]
+        pred_proba = np.array([list(d.values()) for d in pred_onx])
 
-        # post_metrics(project_id, metrics)
+        metrics = prediction_test(
+            project,
+            x_new,
+            project.dataset.date_feature,
+            project.dataset.target,
+            pred_proba,
+        )
+
+        for m in metrics:
+            m.dataset_id = project.dataset.id
+        get_logger().info(f"Saving {len(metrics)} metrics.")
+
+        post_metrics(project_id, metrics)
     else:
-        print(f"Failed to fetch data {response.status_code}")
+        get_logger().error(f"Failed to fetch data {response.status_code}")
 
 
 if __name__ == "__main__":
     evaluate_dataset(1)
-    evaluate_dataset(1)
+    get_logger().info("Task done, exiting.")
